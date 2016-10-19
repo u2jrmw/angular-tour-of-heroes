986363524d6ba23b16c5e6442d6c5b37ac60de85
willis <willis@WILLISJ>
2016-10-19 09:27:53 -0500

    Stuff

--- angular-tour-of-heroes/app/app.module.ts
+++ angular-tour-of-heroes/app/app.module.ts
@@ -1,35 +1,17 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
-
-import { AppComponent }        from './app.component';
-import { DashboardComponent }  from './dashboard.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './hero.service';
-
-
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            }
-
-        ])
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
@@ -37,10 +19,7 @@ import { HeroService }         from './hero.service';
         HeroDetailComponent,
         HeroesComponent
     ],
-    providers: [
-        HeroService
-    ],
+    providers: [HeroService],
     bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
--- angular-tour-of-heroes/app/dashboard.component.ts
+++ angular-tour-of-heroes/app/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -11,15 +13,24 @@ import { HeroService } from './hero.service';
 
 export class DashboardComponent implements OnInit {
 
+
     heroes: Hero[] = [];
 
-    constructor(private heroService: HeroService) { }
+    constructor(
+        private router: Router,
+        private heroService: HeroService) {
+    }
+
 
     ngOnInit(): void {
         this.heroService.getHeroes()
             .then(heroes => this.heroes = heroes.slice(1, 5));
     }
 
-    gotoDetail(hero: Hero): void { /* not implemented yet */ }
+    gotoDetail(hero: Hero): void {
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
+    }
+
 }
 
